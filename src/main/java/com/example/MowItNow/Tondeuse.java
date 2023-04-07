a7bb28ff97def1a0ce4227f222f83403eba4844b
Julien <74259883+JulienFerez@users.noreply.github.com>
2023-04-07 19:05:29 +0200

    add test

--- src/main/java/com/example/MowItNow/Tondeuse.java
+++ src/main/java/com/example/MowItNow/Tondeuse.java
@@ -1,64 +1,58 @@
 package com.example.MowItNow;
 
+import lombok.Getter;
+
+@Getter
 public class Tondeuse {
 
 
 
     public static char pivoterDroite(char orientation) {
-        switch (orientation) {
-            case 'N':
-                return 'E';
-            case 'E':
-                return 'S';
-            case 'S':
-                return 'W';
-            case 'W':
-                return 'N';
-            default:
-                return orientation;
-        }
+        return switch (orientation) {
+            case 'N' -> 'E';
+            case 'E' -> 'S';
+            case 'S' -> 'W';
+            case 'W' -> 'N';
+            default -> orientation;
+        };
     }
 
     public static char pivoterGauche(char orientation) {
-        switch (orientation) {
-            case 'N':
-                return 'W';
-            case 'E':
-                return 'N';
-            case 'S':
-                return 'E';
-            case 'W':
-                return 'S';
-            default:
-                return orientation;
-        }
+        return switch (orientation) {
+            case 'N' -> 'W';
+            case 'E' -> 'N';
+            case 'S' -> 'E';
+            case 'W' -> 'S';
+            default -> orientation;
+        };
     }
 
     public static int[] avancer(int x, int y, char orientation) {
         int[] newPosition = new int[2];
         switch (orientation) {
-            case 'N':
+            case 'N' -> {
                 newPosition[0] = x;
                 newPosition[1] = y + 1;
-                break;
-            case 'E':
+            }
+            case 'E' -> {
                 newPosition[0] = x + 1;
                 newPosition[1] = y;
-                break;
-            case 'S':
+            }
+            case 'S' -> {
                 newPosition[0] = x;
                 newPosition[1] = y - 1;
-                break;
-            case 'W':
+            }
+            case 'W' -> {
                 newPosition[0] = x - 1;
                 newPosition[1] = y;
-                break;
-            default:
+            }
+            default -> {
                 newPosition[0] = x;
                 newPosition[1] = y;
-                break;
+            }
         }
         return newPosition;
     }
+
 
 }
--- src/test/java/com/example/MowItNow/MowItNowApplicationTests.java
+++ src/test/java/com/example/MowItNow/MowItNowApplicationTests.java
@@ -3,11 +3,45 @@ package com.example.MowItNow;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+
 @SpringBootTest
 class MowItNowApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testAffichageResultats() {
+		// Capture la sortie standard dans un tableau de bytes
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outputStream));
+
+		// Exécute le programme avec les entrées spécifiées
+		String[] args = {"5 5", "1 2 N", "GAGAGAGAA", "3 3 E", "AADAADADDA"};
+		MowItNowApplication.main(args);
+
+		// Récupère la sortie du programme
+		String output = outputStream.toString().trim();
+
+		// Vérifie que la sortie correspond aux résultats attendus
+		List<String> expectedOutput = Arrays.asList(
+				"Dimensions de la pelouse : 5 x 5",
+				"Position de départ : (1, 2) Orientation : N Instructions : GAGAGAGAA",
+				"Position finale : (1, 3) Orientation : N",
+				"Position de départ : (3, 3) Orientation : E Instructions : AADAADADDA",
+				"Position finale : (5, 1) Orientation : E"
+		);
+		assertLinesMatch(expectedOutput, Arrays.asList(output.split("\\r?\\n")));
 	}
 
 }
+
+
+
+
+
+
+
